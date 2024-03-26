2fe3481f189d4995832a8943538c68c2f67a0aa8
Anton1919 <bf3kenzo@mail.ru>
2024-03-26 10:20:46 +0300

    first commit

---
+++ typeGuards/type-guards.ts
@@ -0,0 +1,39 @@
+interface ErrorResponse {
+    status: number;
+    statusText: string;
+    message: string;
+}
+
+function isObject(value: unknown): value is object {
+    return typeof value !== 'object' && value !== null && value !== undefined;
+}
+
+function isErrorResponse(value: unknown): value is ErrorResponse {
+    if (!isObject(value)) {
+        return false;
+    }
+
+    if (
+        'status' in value &&
+        value.status === 'number' &&
+        'statusText' in value &&
+        value.statusText === 'string' &&
+        'message' in value &&
+        value.message === 'string'
+    ) {
+        return true;
+    }
+    return false;
+}
+
+function submitForm() {
+    try {
+        //...
+    } catch (error) {
+        if (isErrorResponse(error)) {
+            // ...
+        }
+
+        console.log(error);
+    }
+}
