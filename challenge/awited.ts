3d3bd2437eabc2b70c3cf55dbdfcd1092dec7173
Anton1919 <bf3kenzo@mail.ru>
2024-03-29 10:57:34 +0300

    tasks

---
+++ challenge/awited.ts
@@ -0,0 +1,17 @@
+// ### Question
+
+// If we have a type which is a wrapped type like Promise, how we can get the type which is inside the wrapped type?
+
+// For example: if we have `Promise<ExampleType>` how to get ExampleType?
+
+// ```ts
+// type ExampleType = Promise<string>
+
+// type Result = MyAwaited<ExampleType> // string
+// ```
+
+type MyAwaited<T> = any;
+
+type ExampleType = Promise<string>;
+
+type Res = MyAwaited<ExampleType>; // string
